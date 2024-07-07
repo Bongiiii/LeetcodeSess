2b090021f86d43ee50cd0ea262f5cebf915295a4
Bongiiii <bongiwesan.mkwananzi@students.cookman.edu>
2024-07-07 22:41:12 +0000

    Code challenge submission for question: 50 Pow(x, n)

---
+++ 50 Pow(x, n)/50powx-n.py
@@ -0,0 +1,21 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        #edge case
+        if n == 0:
+            return 1
+            
+        # base case
+        elif n < 0:
+            x = 1 / x
+            n = -n
+
+        result = 1
+        current_product = x
+
+        while n > 0:
+            if n % 2 == 1:
+                result *= current_product
+            current_product *= current_product
+            n //= 2
+
+        return result
