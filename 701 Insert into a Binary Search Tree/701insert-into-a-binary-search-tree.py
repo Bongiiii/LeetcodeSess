1edbe8c7bd4a0da61cf94565f7731c78f050d6ed
Bongiiii <bongiwesan.mkwananzi@students.cookman.edu>
2024-07-25 05:07:52 +0000

    Code challenge submission for question: 701 Insert into a Binary Search Tree

---
+++ 701 Insert into a Binary Search Tree/701insert-into-a-binary-search-tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        #base case, empty tree
+        if not root:
+            return TreeNode(val)
+        
+        # Recursively traverse the tree in pre-order fashion
+        if root.val > val:
+            # If node.val > input value: traverse the left subtree
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            # Else traverse the right subtree
+            root.right = self.insertIntoBST(root.right, val)
+        
+        # Return the root of the tree
+        return root
