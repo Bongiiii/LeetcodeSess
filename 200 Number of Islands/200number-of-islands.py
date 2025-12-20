3adaffd3bade204648d9ab10305540ea3ae3470e
Bongiiii <bongiwesan.mkwananzi@students.cookman.edu>
2025-12-20 21:29:47 +0000

    Code challenge submission for question: 200 Number of Islands

--- 200 Number of Islands/200number-of-islands.py
+++ 200 Number of Islands/200number-of-islands.py
@@ -1,20 +1,37 @@
+from collections import deque
+from itertools import pairwise
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-    
-	# keep track of row and column lengths of grid to access later
-        m = len(grid)
-        n = len(grid[0])
+        """
+        BFS approach/ iterative approach
+        """
+        rows, cols, islandCount, travelDirections = len(grid), len(grid[0]), 0, (-1, 0, 1, 0, -1)
+        
+        #base case unnecessary since constraints guarranteed presence of grid with either 0s or 1s
+        # if not grid or not grid[0]:
+        #     return 0
 
-        def destroyIsland(r,c): # recursive DFS helper
-            grid[r][c] = "2" # set this spot to a different value to avoid infinite loops
-            for (row,col) in [(r-1,c),(r+1,c),(r,c-1),(r,c+1)]: # 4-dimensionally adjacent locations
-                if 0 <= row < m and 0 <= col < n and grid[row][col] == "1": # if not out of bounds and part of the island
-                    destroyIsland(row,col) # destroy the rest of the island (set to "2")
+        for row in range(rows):
+            for col in range(cols):
 
-        ans = 0 # answer
-        for i, row in enumerate(grid): # get rows from grid
-            for j, element in enumerate(row): # go through the row
-                if element == "1": # if we hid land
-                    destroyIsland(i,j) # destroy this island
-                    ans += 1 # we found an island
-        return ans
+                if grid[row][col] == "1":
+                    islandCount += 1
+
+                    #add that cell and its neighbors to queue
+                    queue = deque([(row, col)])
+                    #sink that cell/ mark as visited
+                    grid[row][col] = "2"
+
+                    while queue:
+                        rq, cq = queue.popleft()
+
+                        for r,c in pairwise(travelDirections):
+                            nrow, ncol = rq + r, cq + c
+
+                            if 0<=nrow<rows and 0<=ncol<cols and grid[nrow][ncol] == "1":
+                                grid[nrow][ncol] = "2"
+                                queue.append((nrow, ncol))
+
+
+        return islandCount
