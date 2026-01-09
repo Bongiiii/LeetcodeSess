7ebc7c33ab35e3a45e05d50c7c0d81c4955f9af5
Bongiiii <bongiwesan.mkwananzi@students.cookman.edu>
2026-01-09 19:29:56 +0000

    Code challenge submission for question: 46 Permutations

--- 46 Permutations/46permutations.py
+++ 46 Permutations/46permutations.py
@@ -1,44 +1,29 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         """
-        Understand:
-        input - array of integers, output - array of arrays
-
-        Match:
-        math
-        array manipulation
-        permutations
-        backtracking
-
-        Plan:
-        initiate an array to store resultant array
-        have a helper function to permutate the array
-
-
-        R/E:
-        s/c = O(N * (N-1))
-        t/c = 
+        
         """
-        #array to store resultant array of arrays
-        res = []
+        def dfs(i):
+            if i >= len(nums):
+                #add a copy of subset
+                res.append(temp[:])
+                return
 
+            for j, num in enumerate(nums):
+                if not used[j]:
+                    #mark as used
+                    used[j] = True
 
-        #helper function to recursively backtrack and find permutations of nums
-        def permuteHelper(variation):
-            #base case, variations len is already same length as nums
-            if len(variation) == len(nums):
-                res.append(variation[:])
-                return 
+                    #add current element to subset
+                    temp[i] = num
 
-            #still need to build permutations
-            for num in nums:
-                #use the numbers that have not been used
-                if num not in variation:
-                    variation.append(num)
-                    permuteHelper(variation)
-                    #remove that number and try another
-                    variation.pop()
-                    
-        #iitialize the variation array
-        permuteHelper([])
+                    #recursively fill the next position
+                    dfs(i+1)
+
+                    #mark as unused
+                    used[j] = False
+
+        temp, res, used = [0] * len(nums), [], [False] * len(nums)
+        dfs(0)
+
         return res
