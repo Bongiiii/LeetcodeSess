be699511b701064d159ae5288500a143871b1a50
Bongiiii <bongiwesan.mkwananzi@students.cookman.edu>
2026-01-09 17:59:35 +0000

    Code challenge submission for question: 78 Subsets

--- 78 Subsets/78subsets.py
+++ 78 Subsets/78subsets.py
@@ -1,33 +1,36 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         """
-        Understand:
-        input - array of integers, output = array of arrays of integers
-        powerset result should have 2^n(number of elements in array or len of array)
-
         Match:
-        math
-        array manipulation
-        backtracking
+        backtrack using dfs/recursion
 
         Plan:
-        initialize an array to store resultant array, let it have an empty array in it already
-        have a helper function that uses backtracking to create subsets of the array elements
-
+        start with a temp subset thats empty
+        recursively update subset by either including or excluding an element
+        then backtrack by popping current element
 
         R/E:
-        s/c = t/c = O(N)
+        s/c = O(n)
+        t/c = O(n*2^n) - 
         """
-        res = []
-        
-        def backtrackHelper(start, path):
-            if start == len(nums):
-                res.append(path)
-                return
+        def dfs(i):
+            if i == len(nums):
+                #add copy of the subset
+                res.append(temp[:])
+                return 
 
-            backtrackHelper(start + 1, path + [nums[start]])
-            backtrackHelper(start + 1, path)
+            #exclude curr element
+            dfs(i+1)
 
-        # Start backtracking with an empty subset
-        backtrackHelper(0, [])
+            #include curr element
+            temp.append(nums[i])
+            dfs(i+1)
+
+            #backtrack by removing curr element
+            temp.pop()
+
+        res, temp = [], []
+
+        dfs(0)
+
         return res
