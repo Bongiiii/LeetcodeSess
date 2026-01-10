c94db2dbf527a05444a1afd024c145003bbe8d67
Bongiiii <bongiwesan.mkwananzi@students.cookman.edu>
2026-01-10 22:05:45 +0000

    Code challenge submission for question: 322 Coin Change

--- 322 Coin Change/322coin-change.py
+++ 322 Coin Change/322coin-change.py
@@ -1,33 +1,21 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        """
-        Understand:
-        input - array, integer, and output - integer
+        n, m = len(coins), amount
 
-        Match:
-        array
-        dp
-        math
+        #initiate a dp array that keeps the coins in index 0 and amount in index 1
+        dp = [[float('inf')]*(m+1) for _ in range(n + 1)]
+        dp[0][0] = 0
 
-        Plan:
-        iterate through the denominations/coins
-        for each coin try to make change using the least amount of coins possible
-        initiate a dp array of size amount + 1 and initialize all in the array to float('inf')
-        the zero index element will be set to 0 
-        then populate rest of the dp array using the coins
-        iterate each coin and for each coin check if it is posible to use that coin and still have a remainder, and find the coins to use to breach the remainder gap
-        update/fill dp array from [1:] using the minimum between the current element at that index and the previous dp address and also add the coin you are currently using to make change
-        finally return the last element in the dp if it's not float('inf') else return -1 instead to show that the amount of money cannot be made from that combination
+        for coin in range(1,n+1):
+            curr_coin = coins[coin-1]
 
-        R/E:
-        s/c = O(N), n = amount + 1
-        t/c = O(N*C), c = length of coins
-        """
-        dp = [float('inf') for _ in range(amount+1)]
-        dp[0] = 0
+            for curr_amount in range(m + 1):
+                #don't use curr coin
+                dp[coin][curr_amount] = dp[coin - 1][curr_amount]
 
-        for coin in coins:
-            for x in range(coin, amount + 1):
-                dp[x] = min(dp[x], dp[x - coin] + 1)
+                #use current coin
+                if curr_amount >= curr_coin:
 
-        return dp[-1] if dp[-1] != float('inf') else -1
+                    #explore if you can use one more coin too
+                    dp[coin][curr_amount] = min(dp[coin][curr_amount], dp[coin][curr_amount - curr_coin]+1)
+        return -1 if dp[n][m] == float('inf') else dp[n][m]
