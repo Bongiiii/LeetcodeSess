f0d0dcc92a086834b469815aeb238ca5855c2ce4
Bongiiii <bongiwesan.mkwananzi@students.cookman.edu>
2026-01-17 20:23:07 +0000

    Code challenge submission for question: 56 Merge Intervals

--- 56 Merge Intervals/56merge-intervals.py
+++ 56 Merge Intervals/56merge-intervals.py
@@ -1,11 +1,27 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        #start by sorting the intervals using the start time as the order of sorting
+        intervals.sort(key=lambda x:x[0])
+        
+        #make res array
         res = []
-        # Go through all intervals in sorted order
-        for low, high in sorted(intervals):
-            if not res or res[-1][1] < low:
-                res.append([low, high])  # Start new interval
-            else:
-                res[-1][1] = max(res[-1][1], high)  # Merge intervals
-                
+
+        #grab the start and finish of the first interval
+        cur_start, cur_end = intervals[0]
+        
+        for next_start, next_end in intervals[1:]:
+            #no merge
+            if cur_end < next_start:
+                #add curr start and end to res
+                res.append([cur_start, cur_end])
+
+                #update curr with the next
+                cur_start, cur_end = next_start, next_end
+
+            #they overlap, hence update end of that interval
+            cur_end = max(cur_end, next_end)
+
+        res.append([cur_start, cur_end])
+
         return res
+            
