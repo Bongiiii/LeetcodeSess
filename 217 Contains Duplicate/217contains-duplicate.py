7e83259cf79685a421fe822fefc6ae7f5d032da7
Bongiiii <bongiwesan.mkwananzi@students.cookman.edu>
2026-01-17 18:36:10 +0000

    Code challenge submission for question: 217 Contains Duplicate

--- 217 Contains Duplicate/217contains-duplicate.py
+++ 217 Contains Duplicate/217contains-duplicate.py
@@ -1,11 +1,18 @@
+from collections import Counter
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hashSet = set()
+    #put array in a dict that keeps track of count of each character, key - number and val - frequency
+        freqDict = Counter(nums)
 
+        #iterate through the array
         for num in nums:
-            if num in hashSet:
+            #check freq of num, if > 1 return True
+            if freqDict[num] > 1:
                 return True
 
-            hashSet.add(num)
+            #else continue checking the rest of the array
+            continue
 
         return False
+        
