e099527426737fc314e6c7c54846391f1c71a58d
Bongiiii <bongiwesan.mkwananzi@students.cookman.edu>
2026-02-09 17:19:26 +0000

    Code challenge submission for question: 110 Balanced Binary Tree

--- 110 Balanced Binary Tree/110balanced-binary-tree.py
+++ 110 Balanced Binary Tree/110balanced-binary-tree.py
@@ -4,31 +4,18 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        # Helper function to check balance and compute height
-        def helper(root: Optional[TreeNode]) -> int:
-            # Basecase is a Null Node, return 0
-            if not root:
+        def heightFinder(node):
+            if not node:
                 return 0
+
+            leftt, rightt = heightFinder(node.left), heightFinder(node.right)
+
+            if (leftt == -1) or (rightt == -1) or abs(leftt - rightt) > 1:
+                return -1
+
+            return 1 + max(leftt,rightt)
+
+        return heightFinder(root) >= 0
             
-            # Collect information regarding balance of node
-            leftHeight = helper(root.left)
-            rightHeight = helper(root.right)
-            if abs(rightHeight - leftHeight) > 1:
-                nonlocal balanced
-                balanced = False
-                return 0
-            
-            # Recursively return the max between height of left node and right node and add one for current node.
-            return max(leftHeight, rightHeight) + 1
-
-        # Create variable to hold balance information
-        balanced = True
-
-        # Call the function to check balance of each node
-        helper(root)
-
-        # Return the balance
-        return balanced
